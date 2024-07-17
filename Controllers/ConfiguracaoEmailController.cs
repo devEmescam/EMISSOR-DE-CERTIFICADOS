26fa13f51543b8c5cba92bdcbfe83c0487a65b45
lucasdevemescam <lucas.guimaraes@emescam.brl>
2024-07-17 16:41:09 -0300

    AJUSTES BACK END 170724 --  EmailService: ajustes no retorno do objeto EmailConfigModel e ajuste no endereço da URL do end points consumidos -- EventoPessoasRepository.CarregarDadosAsync:  adicionado novo parametro e ajuste na consulta SQL -- EmailConfigRepository.CarregarDados: não retorna mais lista e sim um unico objeto do tipo definido -- Home_OrganizadorController.GerarCertifcado: ajuste de trucamento de conteudo a ser atribuido em variavel -- adicionado ConfiguracaoEmailController

---
+++ Controllers/ConfiguracaoEmailController.cs
@@ -0,0 +1,48 @@
+﻿using EMISSOR_DE_CERTIFICADOS.Models;
+using EMISSOR_DE_CERTIFICADOS.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMISSOR_DE_CERTIFICADOS.Controllers
+{
+    public class ConfiguracaoEmailController : Controller
+    {
+        private readonly EmailConfigRepository _emailConfigRepo;
+        public ConfiguracaoEmailController(EmailConfigRepository emailConfigRepo) 
+        {
+            _emailConfigRepo = emailConfigRepo;
+        }
+
+        #region *** IActionResults ***        
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var configuracao = await BuscarConfiguracaoAsync();
+                //TO DO: definir direcionamento correto
+                return View(configuracao);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Ocorreu um erro em [ConfiguracaoEmail.Index] Erro: {ex.Message}");                
+            }            
+        }
+        #endregion
+
+        #region *** Metodo ***        
+        private async Task<EmailConfigModel> BuscarConfiguracaoAsync() 
+        {
+            var emailConfigModel = new EmailConfigModel();
+
+            try
+            {
+                return emailConfigModel = await _emailConfigRepo.CarregarDadosAsync();                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu um erro em [ConfiguracaoEmail.BuscarConfiguracaoAsync] Erro: {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
